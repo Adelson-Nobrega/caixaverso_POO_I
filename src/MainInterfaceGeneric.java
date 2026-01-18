fada91cec44706f52667f0f81ed9e5e66ec9deb3
Adelson-Nobrega <lopes_nobre@yahoo.com.br>
2026-01-18 00:02:42 -0300

    Complementação da aula 3

---
+++ src/MainInterfaceGeneric.java
@@ -0,0 +1,25 @@
+import implement.Airplane;
+import implement.Duck;
+import interfaces.Flyable;
+import interfaces.Walkable;
+
+public class MainInterfaceGeneric {
+
+    public static void main(String[] args) {
+        Airplane airplane = new Airplane();
+        makeFly(airplane);
+
+        Duck duck = new Duck();
+        makeFly(duck);
+        makeWalk(duck);
+    }
+
+    public static void makeFly(Flyable flyable) {
+        flyable.fly();
+        flyable.land();
+    }
+
+    public static void makeWalk(Walkable walkable) {
+        walkable.walk();
+    }
+}
